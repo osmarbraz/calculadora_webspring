5c113e508ec8e75d5905e6c6a2cbf8fb4fed6f54
Osmar de Oliveira Braz Junior <osmarbraz@hotmail.com>
2023-05-17 10:48:32 -0300

    Aumento cobertura.

--- src/test/java/com/formulario/TestExpressaoFrm.java
+++ src/test/java/com/formulario/TestExpressaoFrm.java
@@ -6,6 +6,9 @@ import org.junit.jupiter.api.Test;
 
 class TestExpressaoFrm {
 
+    /**
+     * Teste de valores da mensagem de ExpressaoFrm.
+     */
     @Test
     void testExpressaoFrmMensage() {
         ExpressaoFrm instancia = new ExpressaoFrm();
@@ -13,6 +16,9 @@ class TestExpressaoFrm {
         assertEquals("mensagem", instancia.getMensagem());
     }
     
+    /**
+     * Teste de valores de ExpressaoFrm.
+     */
     @Test
     void testExpressaoFrm() {
         ExpressaoFrm instancia = new ExpressaoFrm();
@@ -21,13 +27,4 @@ class TestExpressaoFrm {
         instancia.setOperacao("adicao");
         assertTrue( (instancia.getValorA()==1) && (instancia.getValorB()==2) && ("adicao".equals(instancia.getOperacao())));
     }
-    
-//    @Test
-//    void testExpressaoFrmToString() {
-//        ExpressaoFrm instancia = new ExpressaoFrm();
-//        instancia.setValorA(1);
-//        instancia.setValorB(2);
-//        instancia.setOperacao("adicao");
-//        assertEquals("valorA: 1.0 - valorB: 2.0 - Operação: adicao", instancia);
-//    }
 }
